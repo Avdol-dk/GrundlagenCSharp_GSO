7d3952184fb7c6fbcdba9c4bc104b5c318604030
Deniz Dersim Kest <denizdersi.k@gso.schule.koeln>
2022-03-09 10:36:14 +0100

    Fortinati or babajee? BABAJEE!

---
+++ 2D_Arrays_Aufgaben/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2D_Arrays_Aufgaben
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
