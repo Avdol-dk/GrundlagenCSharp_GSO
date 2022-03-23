5bae05b94ee34a1dbae2e80dfa5818c4d277a611
Deniz Dersim Kest <denizdersi.k@gso.schule.koeln>
2022-03-23 12:51:23 +0100

    dsada

--- Methoden_Aufgabe/Program.cs
+++ Methoden_Aufgabe/Program.cs
@@ -6,91 +6,119 @@ namespace Methoden_Aufgabe
     {
         static void Main(string[] args)
         {
-            int zahl = 0;
-
-            int anz_zeichen = 0;
-            Console.WriteLine("geben Sie einen Text ein");
-
-            bool Texterror = false;
-            string textsave = "a";
-
+            bool EingabeEingabe = false;
             do
             {
-                string Text = Console.ReadLine();
-              
-                    
+                int zahl = 0;
+
+                int anz_zeichen = 0;
+                Console.WriteLine("geben Sie einen Text ein");
+
+                bool Texterror = false;
+                string textsave = "a";
+
+                do
+                {
+                    string Text = Console.ReadLine();
 
 
-                        if (Text == "")
-                        {
-                            Console.WriteLine("Die Eingabe war inkorrekt");
-                            Texterror = true;
 
 
-                        }
-                        else
-                        {
-                            Console.WriteLine("korrekte eingabe!");
-                            Texterror = false;
-                            textsave = Text;
+                    if (Text == "")
+                    {
+                        Console.WriteLine("Die Eingabe war inkorrekt");
+                        Texterror = true;
 
-                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("korrekte eingabe!");
+                        Texterror = false;
+                        textsave = Text;
+
+                    }
+
+
+                } while (Texterror == true);
+
+
+                Console.WriteLine("geben sie an, wie oft es durchlaufen werden soll");
+
+                bool durchlauferror = false;
+                int durchlaufsave = 0;
+
+                do
+                {
+                    string durchlauf = Console.ReadLine();
+
+
+
+                    if (int.TryParse(durchlauf, out zahl))
+                    {
+                        Console.WriteLine("Es wird {0} mal durchlaufen.", durchlauf);
+                        durchlauferror = false;
+                        durchlaufsave = int.Parse(durchlauf);
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("geben sie etwas richtiges  für den Durchlauf ein!");
+                        durchlauferror = true;
+
+                    }
+
+
+                } while (durchlauferror == true);
+                Console.WriteLine("sie haben alle werte eingegeben, führen sie fort indem sie eine beliebige Taste drücken.");
+                Console.ReadKey();
+
+                Console.Clear();
+
+                anz_zeichen = ausgabeTextMalX(textsave, durchlaufsave);
+
+                Console.Write("\nAnzahl der Buchstaben: {0}", anz_zeichen);
+
+
+
+
+
+
+                static int ausgabeTextMalX(string i, int x)
+                {
+
+
+                    for (int dl = 0; dl < x; dl++)
+                    {
+                        Console.WriteLine(i);
+                    }
+                    int anz_zeichen = i.Length * x;
+
+                    return anz_zeichen;
+
+                }
+                Console.WriteLine("wollen sie das Progamm wiederholen? 1 ist ja und 2 nein");
                 
+                bool EingabeError = false;
+                string Eingabe;
 
-            } while (Texterror == true);
-
-
-            Console.WriteLine("geben sie an, wie oft es durchlaufen werden soll");
-
-            bool durchlauferror = false;
-            int durchlaufsave = 0;
-
-            do
-            {
-                string durchlauf = Console.ReadLine();
-
-
-
-                if (int.TryParse(durchlauf, out zahl))
+                do
                 {
-                    Console.WriteLine("Es wird {0} mal durchlaufen.", durchlauf);
-                    durchlauferror = false;
-                    durchlaufsave = int.Parse(durchlauf);
-
-                }
-                else
-                {
-                    Console.WriteLine("geben sie etwas richtiges  für den Durchlauf ein!");
-                    durchlauferror = true;
-
-                }
-
-
-            } while (durchlauferror == true);
-            Console.WriteLine("sie haben alle werte eingegeben, führen sie fort indem sie eine beliebige Taste drücken.");
-            Console.ReadKey();
-
-            Console.Clear();
-
-            anz_zeichen = ausgabeTextMalX(textsave, durchlaufsave);
-
-            Console.Write("\nAnzahl der Buchstaben: {0}", anz_zeichen);
-
-
-
-            static int ausgabeTextMalX(string i, int x)
-            {
-
-
-                for (int dl = 0; dl < x; dl++)
-                {
-                    Console.WriteLine(i);
-                }
-                int anz_zeichen = i.Length * x;
-
-                return anz_zeichen;
-
-            }
+                    Eingabe = Console.ReadLine();
+                    switch(Eingabe)
+                    {
+                        case "1":
+                            
+                                EingabeEingabe = true;
+                            break;
+                            
+                        default:
+                            
+                                Console.Clear();
+                            break;
+                    }
+                } while (EingabeError == false);
+            } while (EingabeEingabe == true);
 
         }
 
