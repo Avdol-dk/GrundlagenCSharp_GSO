73f7f594edef88cb83bf3657f050844cfcb34cb1
Deniz Dersim Kest <denizdersi.k@gso.schule.koeln>
2022-03-30 12:53:22 +0200

    just kiddng, women . Fortnite -------------- L DANCE

---
+++ Methoden implementieren und aufrufen/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Methoden_implementieren_und_aufrufen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            short[] Zimmernummer = { 45, 23, 3, 32 };
+            string[] Vorname = { "John", "Paul", "George", "Ringo" };
+            string[] Nachname = { "Lennon", "McCartney", "Harrison", "Starr" };
+            short[] Übernachtungen = { 1, 4, 12, 3 };
+            double[] Rechnungsbetrag = { 82.46, 329.84, 989.52, 247.38 };
+            bool zimmercheck = false;
+            int i = 0;
+            string eingabe_string;
+            short eingabe_short = 0;
+            bool errorcheck = false;
+            do
+            {
+
+                Console.WriteLine("geben sie einen Wert für Eingabe ein");
+                eingabe_string = Console.ReadLine();
+                
+
+
+                bool flag = short.TryParse(eingabe_string, out eingabe_short);
+                do
+                {
+
+
+
+                    if (flag == true)
+                    {
+
+
+
+
+                        zimmercheck = Zimmernummer[i] == eingabe_short;
+                        if (zimmercheck == true)
+                        {
+                            Console.WriteLine("hat geklappt");
+                            CheckInProgamm(Zimmernummer[i], Vorname[i], Nachname[i], Übernachtungen[i], Rechnungsbetrag[i]);
+                        }
+                        else if (i < Zimmernummer.Length - 1)
+                        {
+                            Console.WriteLine("i++");
+                            ++i;
+
+                        }
+                        else if (i >= Zimmernummer.Length - 1)
+                        {
+                            Console.WriteLine("i ist zu viel");
+                            Console.WriteLine("Geben sie einen neuen Wert für ihre Eingabe ein ");
+                            errorcheck = true;
+
+                        }
+                    }
+
+                } while (zimmercheck == false);
+
+            } while (errorcheck == true);
+        }
+
+
+
+        static (short, string, string, short,double) CheckInProgamm(short Zimmernummer, string Vorname, string Nachname, short Übernachtungen, double Rechnungsbetrag)
+        {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            return (1, "1","1",1,1);
+        }
+
+
+    }
+}
--- Methoden_Aufgabe/Program.cs
+++ Methoden_Aufgabe/Program.cs
@@ -8,99 +8,76 @@ namespace Methoden_Aufgabe
         {
 
             {
-                int zahl = 0;
-
-                int anz_zeichen = 0;
-                Console.WriteLine("geben Sie einen Text ein");
-
-                bool Texterror = false;
-                string textsave = "a";
-
+                bool Progammwiederholen = true;
                 do
                 {
-                    string Text = Console.ReadLine();
-
-
-
-
-                    if (Text == "")
+                    int zahl = 0;
+                    int anz_zeichen = 0;
+                    Console.WriteLine("geben Sie einen Text ein");
+                    bool Texterror = false;
+                    string textsave = "a";
+                    do
                     {
-                        Console.WriteLine("Die Eingabe war inkorrekt");
-                        Texterror = true;
-
-
-                    }
-                    else
+                        string Text = Console.ReadLine();
+                        if (Text == "")
+                        {
+                            Console.WriteLine("Die Eingabe war inkorrekt");
+                            Texterror = true;
+                        }
+                        else if(Text == "easteregg")
+                        {
+                            System.Diagnostics.Process.Start("chrome.exe", "https://www.youtube.com/watch?v=rhvF2_JkDhQ");
+                            Texterror = true;
+                            Console.WriteLine("HAHA, bitte neue Eingabe");
+                        }
+                        else
+                        {
+                            Console.WriteLine("korrekte eingabe!");
+                            Texterror = false;
+                            textsave = Text;
+                        }
+                    } while (Texterror == true);
+                    Console.WriteLine("geben sie an, wie oft es durchlaufen werden soll");
+                    bool durchlauferror = false;
+                    int durchlaufsave = 0;
+                    do
                     {
-                        Console.WriteLine("korrekte eingabe!");
-                        Texterror = false;
-                        textsave = Text;
-
-                    }
-
-
-                } while (Texterror == true);
-
-
-                Console.WriteLine("geben sie an, wie oft es durchlaufen werden soll");
-
-                bool durchlauferror = false;
-                int durchlaufsave = 0;
-
-                do
-                {
-                    string durchlauf = Console.ReadLine();
-
-
-
-                    if (int.TryParse(durchlauf, out zahl))
-                    {
-                        Console.WriteLine("Es wird {0} mal durchlaufen.", durchlauf);
-                        durchlauferror = false;
-                        durchlaufsave = int.Parse(durchlauf);
-
-                    }
-                    else
-                    {
-                        Console.WriteLine("geben sie etwas richtiges  für den Durchlauf ein!");
-                        durchlauferror = true;
-
-                    }
+                        string durchlauf = Console.ReadLine();
+                        if (int.TryParse(durchlauf, out zahl))
+                        {
+                            Console.WriteLine("Es wird {0} mal durchlaufen.", durchlauf);
+                            durchlauferror = false;
+                            durchlaufsave = int.Parse(durchlauf);
+                        }
+                        else
+                        {
+                            Console.WriteLine("geben sie etwas richtiges  für den Durchlauf ein!");
+                            durchlauferror = true;
+                        }
+                    } while (durchlauferror == true);
+                    Console.WriteLine("sie haben alle werte eingegeben, führen sie fort indem sie eine beliebige Taste drücken.");
+                    Console.ReadKey();
+                    Console.Clear();
+                    anz_zeichen = ausgabeTextMalX(textsave, durchlaufsave);
+                    Console.WriteLine("\nAnzahl der Buchstaben: {0}", anz_zeichen);
+                    
+                } while (Progammwiederholen == true);
 
 
-                } while (durchlauferror == true);
-                Console.WriteLine("sie haben alle werte eingegeben, führen sie fort indem sie eine beliebige Taste drücken.");
-                Console.ReadKey();
-
-                Console.Clear();
-
-                anz_zeichen = ausgabeTextMalX(textsave, durchlaufsave);
-
-                Console.Write("\nAnzahl der Buchstaben: {0}", anz_zeichen);
-
-
-
-
-
-
-                static int ausgabeTextMalX(string i, int x)
-                {
-
-
-                    for (int dl = 0; dl < x; dl++)
-                    {
-                        Console.WriteLine(i);
-                    }
-                    int anz_zeichen = i.Length * x;
-
-                    return anz_zeichen;
-
-                }
-                
-
-                //beendet
             }
-
         }
+        static int ausgabeTextMalX(string i, int x)
+        {
+            for (int dl = 0; dl < x; dl++)
+            {
+                Console.WriteLine(i);
+            }
+            int anz_zeichen = i.Length * x;
+            Console.ReadKey();
+            Console.Clear();
+            return anz_zeichen;
+            
+        }
+        
     }
 }
